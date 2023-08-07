32c53edc96f8c136b278ff86b0b2a2488af0c68f
Asingh0406 <monke110406@gmail.com>
2023-08-07 16:28:40 -0400

    visData

---
+++ data.js
@@ -0,0 +1,182 @@
+let people = [
+    {
+        // "results": [
+        // {
+            "gender": "female",
+            "name": {
+            "title": "Ms",
+            "first": "Emilia",
+            "last": "Wirkkala"
+            },
+            "location": {
+            "street": {
+                "number": 2629,
+                "name": "Otavalankatu"
+            },
+            "city": "Oripää",
+            "state": "Kainuu",
+            "country": "Finland",
+            "postcode": 74455,
+            "coordinates": {
+                "latitude": "-29.6506",
+                "longitude": "1.0735"
+            },
+            "timezone": {
+                "offset": "-10:00",
+                "description": "Hawaii"
+            }
+            },
+            "email": "emilia.wirkkala@example.com",
+            "login": {
+            "uuid": "3fb14893-b60a-4051-8f76-3cc241efdc5b",
+            "username": "greenelephant830",
+            "password": "jolly",
+            "salt": "zaNdtfa0",
+            "md5": "1a9ec21184f3eccff73047ba68b5b56e",
+            "sha1": "e2226ad08e7ee724572cd8fd3525521efa4f18ad",
+            "sha256": "7111b049650b157e8b915d4f89c814ac61730b2a4d13c6d9bd659b48eefd73d5"
+            },
+            "dob": {
+            "date": "1989-12-16T23:47:19.906Z",
+            "age": 33
+            },
+            "registered": {
+            "date": "2005-10-30T13:06:04.179Z",
+            "age": 17
+            },
+            "phone": "02-998-737",
+            "cell": "045-871-37-18",
+            "id": {
+            "name": "HETU",
+            "value": "NaNNA412undefined"
+            },
+            "picture": {
+            "large": "https://randomuser.me/api/portraits/women/24.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/women/24.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/24.jpg"
+            },
+            "nat": "FI"
+        // }
+        // ],
+    },
+    {
+        // "results": [
+        //   {
+            "gender": "male",
+            "name": {
+              "title": "Mr",
+              "first": "Ventura",
+              "last": "Moura"
+            },
+            "location": {
+              "street": {
+                "number": 335,
+                "name": "Rua Alagoas "
+              },
+              "city": "Cabo de Santo Agostinho",
+              "state": "Maranhão",
+              "country": "Brazil",
+              "postcode": 47317,
+              "coordinates": {
+                "latitude": "-73.2883",
+                "longitude": "-17.9073"
+              },
+              "timezone": {
+                "offset": "+5:45",
+                "description": "Kathmandu"
+              }
+            },
+            "email": "ventura.moura@example.com",
+            "login": {
+              "uuid": "fab01256-7d3d-47f8-961b-8769021f9ecd",
+              "username": "beautifulpanda571",
+              "password": "highbury",
+              "salt": "B1m1C9Gv",
+              "md5": "e7f73e6c97eb04205a254bd1c304f404",
+              "sha1": "1b9c02c1bada98bdcf9186883d7cd32f7c2a2f18",
+              "sha256": "4e4f247e46e49bc8d587b3c0c6b657ce848976d386ffc5af8b087b2d9d3c9bd8"
+            },
+            "dob": {
+              "date": "1969-06-25T09:43:42.063Z",
+              "age": 54
+            },
+            "registered": {
+              "date": "2010-04-13T04:03:01.586Z",
+              "age": 13
+            },
+            "phone": "(01) 0958-7883",
+            "cell": "(13) 7418-7999",
+            "id": {
+              "name": "CPF",
+              "value": "254.490.924-21"
+            },
+            "picture": {
+              "large": "https://randomuser.me/api/portraits/men/56.jpg",
+              "medium": "https://randomuser.me/api/portraits/med/men/56.jpg",
+              "thumbnail": "https://randomuser.me/api/portraits/thumb/men/56.jpg"
+            },
+            "nat": "BR"
+        //   }
+        // ],
+      },
+      {
+        // "results": [
+        //   {
+            "gender": "female",
+            "name": {
+              "title": "Miss",
+              "first": "Thea",
+              "last": "Wang"
+            },
+            "location": {
+              "street": {
+                "number": 5147,
+                "name": "East Tamaki Road"
+              },
+              "city": "Greymouth",
+              "state": "Taranaki",
+              "country": "New Zealand",
+              "postcode": 12442,
+              "coordinates": {
+                "latitude": "50.4151",
+                "longitude": "156.9424"
+              },
+              "timezone": {
+                "offset": "-4:00",
+                "description": "Atlantic Time (Canada), Caracas, La Paz"
+              }
+            },
+            "email": "thea.wang@example.com",
+            "login": {
+              "uuid": "fd04833b-05df-4c5b-ba8d-3ecc39ebc777",
+              "username": "ticklishduck943",
+              "password": "lawyer",
+              "salt": "w20WuTRO",
+              "md5": "5a22009e0c909b2c4e7ddb50db799e8b",
+              "sha1": "2ad97da24d4401f6c5b74df3dd54f215a35975a4",
+              "sha256": "728e3574560033544f7e309f01913dbcb12d9e5b95a9ca52feb567a91f8156ae"
+            },
+            "dob": {
+              "date": "1990-01-30T12:26:41.942Z",
+              "age": 33
+            },
+            "registered": {
+              "date": "2018-03-14T09:44:59.394Z",
+              "age": 5
+            },
+            "phone": "(597)-744-4974",
+            "cell": "(030)-082-0026",
+            "id": {
+              "name": "",
+              "value": null
+            },
+            "picture": {
+              "large": "https://randomuser.me/api/portraits/women/18.jpg",
+              "medium": "https://randomuser.me/api/portraits/med/women/18.jpg",
+              "thumbnail": "https://randomuser.me/api/portraits/thumb/women/18.jpg"
+            },
+            "nat": "NZ"
+        //   }
+        // ],
+      }
+]
---
+++ main.js
@@ -0,0 +1,8 @@
+console.log(people[1].name);
+console.log(people[1].gender)
+console.log(people[1].location.street)
+console.log(people[1].email)
+console.log(people[1].dob.date)
+console.log(people[1].dob.age)
+console.log(people[1].cell)
+console.log(people[1].login.password)
